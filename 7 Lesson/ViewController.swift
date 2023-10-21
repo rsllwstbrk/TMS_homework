dbe4d3b5a53b3c965afd93d4ed754728ad64e5b7
Russell <rshtyrev@gmail.com>
2023-10-21 18:44:24 +0300

    Initial Commit

---
+++ 7 Lesson/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  7 Lesson
+//
+//  Created by Rsllwstbrk on 21.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var uu: Int = 1
+        print(uu)
+        
+    }
+
+
+}
+
