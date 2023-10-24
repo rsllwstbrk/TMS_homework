fe7610cf69e75c94f71bed57de14c279f684927a
Russell <rshtyrev@gmail.com>
2023-10-25 00:03:52 +0300

    8 lesson completed

--- 8 Lesson/ViewController.swift
+++ 8 Lesson/ViewController.swift
@@ -7,11 +7,85 @@
 
 import UIKit
 
+class Menu {
+    
+    var name: String = ""
+    var price: Int = 0
+    func food () {}
+}
+
+class snacks: Menu {
+    override init () {
+        super.init()
+        name = "snacks"
+    }
+    override func food () {
+        print(name, price)
+    }
+    
+}
+
+class mainDishes: Menu {
+    override init () {
+        super.init()
+        name = "mainDishes"
+    }
+    override func food () {
+        print(name, price)
+    }
+    
+}
+
+class drinks: Menu {
+    override init () {
+        super.init()
+        name = "drinks"
+    }
+    override func food () {
+        print(name, price)
+    }
+    
+}
+
+class desserts: Menu {
+    override init () {
+        super.init()
+        name = "desserts"
+    }
+    override func food () {
+        print(name, price)
+    }
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let popcorn = snacks()
+        popcorn.name = "Corny"
+        popcorn.price = 12
+        popcorn.food()
+        
+        let wings = mainDishes()
+        wings.name = "Wingy"
+        wings.price = 15
+        wings.food()
+
+        
+        let beer = drinks()
+        beer.name = "Corny"
+        beer.price = 18
+        beer.food()
+
+        
+        let cake = desserts()
+        cake.name = "Moon"
+        cake.price = 20
+        cake.food()
+
+
     }
 
 
