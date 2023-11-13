8ba4f4bae045ab275f7c37a2489f4b81e1c97d16
Russell <rshtyrev@gmail.com>
2023-11-13 20:56:10 +0300

    Menu added to FourthViewController

--- 8 Lesson/FourthViewController.swift
+++ 8 Lesson/FourthViewController.swift
@@ -16,18 +16,43 @@ class FourthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+        
+        
+        var menuItems: [UIAction] {
+            return [
+                UIAction(title: "Chicken, 10$", image: UIImage(systemName: "fork.knife.circle.fill"), handler: { (_) in }),
+                UIAction(title: "Chips, 5$", image: UIImage(systemName: "carrot"), handler: { (_) in }),
+                UIAction(title: "Cola, 8$", image: UIImage(systemName: "cup.and.saucer"), handler: { (_) in }),
+                UIAction(title: "Cake, 9$", image: UIImage(systemName: "birthday.cake"), handler: { (_) in }),
+                UIAction(title: "Borshch, 10$", image: UIImage(systemName: "fork.knife.circle"), handler: { (_) in })
+            ]
+        }
+        
+        var demoMenu: UIMenu {
+            return UIMenu(title: "My menu", image: nil, identifier: nil, options: [], children: menuItems)
+        }
 
-    /*
-    // MARK: - Navigation
+        func showMenuButton() {
+            menuButton.menu = demoMenu
+            menuButton.showsMenuAsPrimaryAction = true
+        }
+        
+       let menuButton = UIButton()
+        view.addSubview(menuButton)
+        menuButton.showsMenuAsPrimaryAction = true
+        menuButton.backgroundColor = .black
+        menuButton.setTitle("Menu", for: .normal)
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
+        menuButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        menuButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        menuButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        menuButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        menuButton.titleLabel?.textColor = .white
+        menuButton.menu = UIMenu(title: "Restaurant Menu", children: menuItems)
+        
+        
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
